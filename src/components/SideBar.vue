93ffd9d0aa4ed052affad47acac1e736b9bce910
Abdul Khan <ahkn63@gmail.com>
2024-08-04 04:30:04 -0400

    Intial Commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineEmits } from "vue"
+const emits = defineEmits(["selectOption"])
+
+const selectOption = (isColor) => {
+  emits("selectOption", isColor)
+}
+</script>
+
+<template>
+  <VNavigationDrawer expand-on-hover rail>
+    <VList>
+      <VListItem
+        prepend-avatar="https://avatars.githubusercontent.com/u/27078948?v=4"
+        title="Abdul Khan"
+        subtitle="ahkn63@gmail.com"
+      ></VListItem>
+    </VList>
+
+    <VDivider />
+
+    <VList density="compact" nav>
+      <VListItem
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        @click="selectOption(true)"
+      />
+    </VList>
+
+    <VList density="compact" nav>
+      <VListItem
+        prepend-icon="mdi-dialpad"
+        title="Greyspace Gallery"
+        value="color"
+        @click="selectOption(false)"
+      />
+    </VList>
+  </VNavigationDrawer>
+</template>
